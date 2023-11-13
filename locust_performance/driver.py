9edcbe62cb6cf01a5b8ced536c4a6e44e045a347
vadimkhaskel <vadim.khaskel@hayden.ai>
2023-11-12 20:56:59 -0800

    adding adding additional cmd line param to driver.py

--- locust_performance/driver.py
+++ locust_performance/driver.py
@@ -19,23 +19,29 @@ def init_routine():
     parser.add_argument('--num-users', dest='num_users', type=int, default=9, help="Number of users")
     parser.add_argument('--spawn-rate', dest='spawn_rate', type=int, default=2, help="User spawn rate")
     parser.add_argument('--run-time', dest='run_time', type=int, default=30, help="Test run time")
+    parser.add_argument('--target-environment', dest='target_environment', type=str, default='Staging',
+                        help="pointing to local aws profile")
     global_args = parser.parse_args()
-    builtins.params = {'num_users': global_args.num_users, 'run_time': global_args.run_time, 'spawn_rate': global_args.spawn_rate}
+    builtins.params = {'num_users': global_args.num_users, 'run_time': global_args.run_time,
+                       'spawn_rate': global_args.spawn_rate, 'target_environment': global_args.target_environment}
 
 
 def run_locust(**kwargs):
     num_users = kwargs.get('num_users')
     run_time = kwargs.get('run_time')
     spawn_rate = kwargs.get('spawn_rate')
+    target_environment = kwargs.get('target_environment')
 
     # Check that all values are not None
     assert num_users is not None, "num_users cannot be None"
     assert run_time is not None, "run_time cannot be None"
     assert spawn_rate is not None, "spawn_rate cannot be None"
+    assert target_environment is not None, "target_environment cannot be None"
 
     try:
         script_path = os.path.join(os.path.dirname(__file__), 'run_locust.sh')
-        subprocess.run(['bash', script_path, str(num_users), str(spawn_rate), str(run_time)], check=True)
+        subprocess.run(['bash', script_path, str(num_users), str(spawn_rate),
+                        str(run_time), str(target_environment)], check=True)
     except subprocess.CalledProcessError as e:
         print(f"Error running 'run_locust.sh': {e}")
     except FileNotFoundError:
